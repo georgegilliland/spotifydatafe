08eff98078ec01fa8da5241794ddad1826444eb6
George Gilland <georgegilliland92@gmail.com>
2021-12-29 17:46:04 +0000

    changes innit

--- src/components/AuthenticateSpotify/AuthenticateSpotify.js
+++ src/components/AuthenticateSpotify/AuthenticateSpotify.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "@mui/material/Button";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+
 import { processData, handleLogin } from "./utils/utils";
 import { useSnackbar } from 'notistack';
 import "./AuthenticateSpotify.css";
 
-export const AuthenticateSpotify = () => { 
+;export const AuthenticateSpotify = () => { 
   const [storageAccessTokenLoaded, setStorageAccessTokenLoaded] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
 
@@ -13,7 +15,7 @@ export const AuthenticateSpotify = () => {
     try {
       const { data } = await axios.get("https://api.spotify.com/v1/me/top/artists?time_range=short_term", {
         headers: {
-          "Authorization": `Bearer ${token}`,
+          "Authorization": `Bearer ${token}a`,
           "Content-Type": "application/json"
         }
       });
@@ -53,6 +55,10 @@ export const AuthenticateSpotify = () => {
             type="submit"
             className="formItem"
             onClick={handleLogin}
+            style={{
+              color: "white",
+              borderColor: "white"
+            }}
           >
               Get your data
           </Button>
