ab48462f3d554674c9ca9f0ca520a7806d93b454
George Gilliland <georgegilliland@Georges-MacBook-Pro-2.local>
2022-01-04 22:03:54 +0000

    added test

---
+++ src/components/AuthenticateSpotify/__tests__/AuthenticateSpotify.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing'
+import AuthenticateSpotify from '../AuthenticateSpotify';
+
+const mockEnqueue = jest.fn();
+
+jest.mock('notistack', () => ({
+    ...jest.requireActual('notistack'),
+    useSnackbar: () => {
+        return {
+            enqueueSnackbar: mockEnqueue
+        };
+    }
+}));
+
+test('renders component', async () => {
+    render(
+        <MockedProvider>
+            <AuthenticateSpotify />
+        </MockedProvider>
+    )
+    const button = screen.getByText(/Get your data/i);
+    expect(button).toBeInTheDocument();
+});
